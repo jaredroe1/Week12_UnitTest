2a4e427ed0a7b188913f459aab16820480bb2826
Jared Roe <jaredroe1@gmail.com>
2023-03-10 09:32:55 -0600

    first commit

--- Week7/src/main/java/projects/service/ProjectService.java
+++ Week7/src/main/java/projects/service/ProjectService.java
@@ -2,6 +2,7 @@ package projects.service;
 
 import projects.dao.ProjectDao;
 import projects.entity.Project;
+import projects.exception.DbException;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -24,5 +25,17 @@ public class ProjectService {
     public Project fetchProjectById(Integer projectId) {
         return projectDao.fetchProjectById(projectId).orElseThrow(() -> new NoSuchElementException(
                 "Project with project ID=" + projectId + " does not exist."));
+    }
+
+    public void modifyProjectDetails(Project project) {
+        if(!projectDao.modifyProjectDetails(project)) {
+            throw new DbException("Project with ID=" + project.getProjectId() + " does not exist.");
+        }
+    }
+
+    public void deleteProject(Integer projectId) {
+        if(!projectDao.deleteProject(projectId)) {
+            throw new DbException("Project with ID=" + projectId + " does not exist.");
+        }
     }
 }
